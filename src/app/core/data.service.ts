310527f8bdf0e89b8d58f2372dae2bc3497214da
Zandre Botha <botha.zandre@gmail.com>
2022-03-08 13:38:41 +0100

    Module 3: Creating and Using Services

---
+++ src/app/core/data.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { allBooks, allReaders } from 'app/data';
+import { Book } from 'app/models/book';
+import { Reader } from 'app/models/reader';
+import { LoggerService } from './logger.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  mostPopularBook: Book = allBooks[0];
+
+  constructor(private loggerService: LoggerService) { }
+
+  setMostPopularBook(book: Book): void {
+    this.mostPopularBook = book;
+  }
+
+  getAllReaders(): Reader[] {
+    return allReaders;
+  }
+
+  getReaderById(id: number): Reader {
+    return allReaders.find(r => r.readerID === id);
+  }
+
+  getAllBooks(): Book[] {
+    return allBooks;
+  }
+
+  getBookById(id: number): Book {
+    return allBooks.find(r => r.bookID === id);
+  }
+}
